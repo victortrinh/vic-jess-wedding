c59ac608111f807859373ea2b15020dd473d9bfe
Victor Trinh <victor.trinh@gsoft.com>
2024-02-17 18:52:33 -0500

    Add container padding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,15 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem"
+      }
+    },
     extend: {}
   },
   plugins: []
