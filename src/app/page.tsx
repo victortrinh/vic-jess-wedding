d8ac480fbf57a129024384274b647c474f2f1f2d
Victor Trinh <victor.trinh@gsoft.com>
2024-02-19 18:55:09 -0500

    Update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Victor and Jessica's Love Story",
     description: " Follow along as Victor and Jessica embark on their journey to happily ever after. Learn more about their love story, wedding details, and special moments shared with family and friends as they celebrate their union.",
-    images: [
-      {
-        url: "https://www.vicandjess.love/hero.jpg",
-        alt: "Jess and Victor engagement"
-      }
-    ]
-  
+    images: ["/hero.jpg"]
   }
 };
 
