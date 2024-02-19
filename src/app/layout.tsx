3c74e08b6791114e6ab6a8a508140bffbca74879
Victor Trinh <victor.trinh@gsoft.com>
2024-02-19 18:43:52 -0500

    Push metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,18 @@ const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
 export const metadata: Metadata = {
   assets: ["hero.jpg"],
   title: "Victor and Jessica's Love Story",
-  description: " Follow along as Victor and Jessica embark on their journey to happily ever after. Learn more about their love story, wedding details, and special moments shared with family and friends as they celebrate their union."
+  description: " Follow along as Victor and Jessica embark on their journey to happily ever after. Learn more about their love story, wedding details, and special moments shared with family and friends as they celebrate their union.",
+  openGraph: {
+    title: "Victor and Jessica's Love Story",
+    description: " Follow along as Victor and Jessica embark on their journey to happily ever after. Learn more about their love story, wedding details, and special moments shared with family and friends as they celebrate their union.",
+    images: [
+      {
+        url: "https://www.vicandjess.love/hero.jpg",
+        alt: "Jess and Victor engagement"
+      }
+    ]
+  
+  }
 };
 
 export default function RootLayout({
